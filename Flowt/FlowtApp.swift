5d49534b613c151cd60e00e689ec340809b3c9dd
Wiktor Drab <LVmorium@wp.pl>
2025-08-20 21:40:25 +0200

    Initial Commit

---
+++ Flowt/FlowtApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowtApp.swift
+//  Flowt
+//
+//  Created by Wiktor Drab on 20/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlowtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
