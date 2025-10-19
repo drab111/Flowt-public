8fce5ea4ac4dcada6aafc226b4be835da7792c84
Wiktor Drab <LVmorium@wp.pl>
2025-10-19 16:39:12 +0200

    new links

--- Flowt/App/ViewModels/InfoViewModel.swift
+++ Flowt/App/ViewModels/InfoViewModel.swift
@@ -10,6 +10,6 @@ import SwiftUI
 @MainActor
 final class InfoViewModel: ObservableObject {
     // MARK: - URLs
-    let termsURL = URL(string: "https://respected-tuck-d26.notion.site/Flowt-27e268ff3eaa8019bc63deeb543e4f91?source")!
-    let privacyURL = URL(string: "https://respected-tuck-d26.notion.site/Flowt-27e268ff3eaa80ccbdffe9b63c1041ca?source")!
+    let termsURL = URL(string: "https://flowt-support.notion.site/Flowt-27e268ff3eaa8019bc63deeb543e4f91")!
+    let privacyURL = URL(string: "https://flowt-support.notion.site/Flowt-27e268ff3eaa80ccbdffe9b63c1041ca")!
 }
